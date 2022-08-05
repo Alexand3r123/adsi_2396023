f237bca68976de55084909ad3d6935f2f6416d0d
Alexand3r123 <Toroalexander29@gmail.com>
2022-08-05 16:20:03 -0500

    PHP

---
+++ 12-php/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?="Main Menu (PHP)"?></title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/fonts.css">
+</head>
+<body>
+    <main class="container">
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+
+                <?php echo "<h1 class='text-center my-5'>Main Menu (PHP) </h1>"; ?>
+                <hr>
+                <div class="list-group">
+                  <a href="01-hello-world"  class="list-group-item list-group-item-action">
+                    <span class="badge rounded-pill bg-dark">01</span> Hello World 
+                </a>
+                <a href="02-php-info"  class="list-group-item list-group-item-action">
+                    <span class="badge rounded-pill bg-dark">02</span> Php Info 
+                </a>
+                <a href="03-comments"  class="list-group-item list-group-item-action">
+                    <span class="badge rounded-pill bg-dark">03</span> Comments
+                </a>
+                <a href="04-Variables"  class="list-group-item list-group-item-action">
+                    <span class="badge rounded-pill bg-dark">04</span> Variables
+                </a>
+                <a href="05-Strings"  class="list-group-item list-group-item-action">
+                    <span class="badge rounded-pill bg-dark">05</span> Strings
+                </a>
+                <a href="06-Operators-Arimetics"  class="list-group-item list-group-item-action">
+                    <span class="badge rounded-pill bg-dark">06</span> Operators Arhimetics
+                </a>
+                <a href="07-operators-assigmet"  class="list-group-item list-group-item-action">
+                    <span class="badge rounded-pill bg-dark">07</span> Operators Assigmet
+                </a>
+                <a href="08-Operators-comparison"  class="list-group-item list-group-item-action">
+                    <span class="badge rounded-pill bg-dark">08</span> Operators Comparison
+                </a>
+                <a href="09-Operators-logics"  class="list-group-item list-group-item-action">
+                    <span class="badge rounded-pill bg-dark">09</span> Operators Logics
+                </a>
+                <a href="10-Conditional-if"  class="list-group-item list-group-item-action">
+                    <span class="badge rounded-pill bg-dark">10</span> Conditional If
+                </div>
+
+            </div>
+        </div>
+    </main>
+    
+
+    <script src="js/bootstrap.min.js"> </script>
+    
+</body>
+</html>
